519e50cc473a70733cb9904f1fb556371c070adb
Maxim Yefremov <maxYefr@gmail.com>
2018-04-21 06:50:46 +0300

    running nginx container

--- nginx.sh
+++ nginx.sh
@@ -1,7 +1,29 @@
 #!/usr/bin/env bash
+set -ex
 
+# https://linode.com/docs/applications/containers/how-to-deploy-nginx-on-a-kubernetes-cluster/#deploy-nginx-on-the-kubernetes-cluster
 create(){
 	kubectl create deployment nginx --image=nginx
+}
+
+get(){
+	kubectl get deployments
+}
+
+describe(){
+	kubectl describe deployment nginx
+}
+
+createSvc(){
+	kubectl create service nodeport nginx --tcp=80:80
+}
+
+svc(){
+	kubectl get svc
+}
+
+delete(){
+	kubectl delete deployment nginx
 }
 
 $@
