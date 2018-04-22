8b1e77b91653629600cf10ed8361fcfd00111d0d
Maxim Yefremov <maxYefr@gmail.com>
2018-04-22 19:49:17 +0300

    more not helpfull  commands to debug CrashLoopBackOff

--- ubuntu.sh
+++ ubuntu.sh
@@ -6,4 +6,22 @@ run(){
 	kubectl run badserver --image=ubuntu:16.04 -l visualize=true,run=badserver
 }
 
+bad2(){
+	kubectl run badserver2 --image=ubuntu -l visualize=true,run=badserver2
+}
+
+bad3(){
+	kubectl run badserver3 --image=teran/ubuntu-network-troubleshooting -l visualize=true,run=badserver3
+}
+
+nginx2(){
+	kubectl run nginx2 --image=nginx -l visualize=true,run=nginx2
+}
+
+docker_ubuntu(){
+	docker run -it ubuntu
+}
+
+
+
 $@
